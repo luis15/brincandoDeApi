de654824238b79303b55ccf6e9f1195f97d86122
luis15 <coelho.luisantonio@hotmail.com>
2017-02-01 23:44:47 -0200

    problemas com parametros no post

--- servidor.js
+++ servidor.js
@@ -9,6 +9,24 @@ app.get('/listar', function (req, res) {
    });
 })
 
+app.post('/adicionar', function (req, res) {
+  console.log(req.body);
+
+   const nomeUsu = req.body.nome;
+   let usu = [];
+   usu.nome = req.body.nome;
+   usu.senha = req.body.senha;
+   usu.cargo = req.body.cargo;
+   usu.id = req.body.id;
+
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+       data = JSON.parse( data );
+       data[nomeUsu] = usu;
+       console.log( data );
+       res.end( JSON.stringify(data));
+   });
+})
+
 var server = app.listen(7777, function () {
 
   var host = server.address().address
